3a11338877b980e758bae2acf5e91baa90811e6d
Ivan Bueno <ivan.bueno@jud.ca.gov>
2021-07-14 10:13:10 -0700

    crawl fresno

--- projects/trial_court_fresno/trial_court_fresno/items.py
+++ projects/trial_court_fresno/trial_court_fresno/items.py
@@ -4,9 +4,22 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import Join, MapCompose, Identity
+from w3lib.html import remove_tags
+from .utils.processors import Text, Number, Price, Date, Url, Image
 
 
 class TrialCourtFresnoItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    url = scrapy.Field(
+        input_processor=Url(),
+        output_processor=Join(),
+    )
+    title = scrapy.Field(
+        input_processor=Text(),
+        output_processor=Join(),
+    )
+    body = scrapy.Field()
+    parent = scrapy.Field(
+        input_processor=Url(),
+        output_processor=Join(),
+    )
--- projects/trial_court_fresno/trial_court_fresno/spiders/www_fresno_courts_ca_gov.py
+++ projects/trial_court_fresno/trial_court_fresno/spiders/www_fresno_courts_ca_gov.py
@@ -1,20 +1,32 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from ..items import TrialCourtFresnoItem
 
 
 class WwwFresnoCourtsCaGovSpider(CrawlSpider):
     name = 'www.fresno.courts.ca.gov'
     allowed_domains = ['www.fresno.courts.ca.gov']
-    start_urls = ['http://www.fresno.courts.ca.gov/']
+    start_urls = [
+        'http://www.fresno.courts.ca.gov/',
+        'http://www.fresno.courts.ca.gov/case_info/'
+    ]
 
-    rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-    )
+    rules = [
+        Rule(
+            LinkExtractor(
+                allow=('.*'),
+                deny=()
+            ),
+            callback='parse_item',
+            follow=True
+        )
+    ]
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        item = TrialCourtFresnoItem()
+        item['url'] = response.url
+        item['title'] = response.css('h1::text').get().strip()
+        item['body'] = '<table><tr>' + response.xpath("/html/body/table/tr/td[2]/table[4]/tr[3]/td[2]").get().strip() + '</tr></table>'
+        item['parent'] = 'http://www.fresno.courts.ca.gov' + response.css('a.breadcrumb:nth-last-child(2)::attr(href)').get().strip()
+        yield item
